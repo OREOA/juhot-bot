d4ccfa1673d9f71886bf22275527448d12813b6d
Aapeli Haanpuu <aapzu@iki.fi>
2019-11-22 13:45:53 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,5 @@
-const API_URL = process.env.API_URL
-const BASE_URL = process.env.BASE_URL
-const ENABLE_FREE_TEXT = process.env.ENABLE_FREE_TEXT
-const FREE_TEXT_IN_PRIVATE_ONLY = process.env.FREE_TEXT_IN_PRIVATE_ONLY
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
-const WIT_TOKEN = process.env.WIT_TOKEN
 
 export default {
-    API_URL,
-    BASE_URL,
-    ENABLE_FREE_TEXT,
-    FREE_TEXT_IN_PRIVATE_ONLY,
     TELEGRAM_TOKEN,
-    WIT_TOKEN,
 }
