06b0242937d098d549680a595606920f973e1cda
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-11-22 14:55:33 +0200

    command poika

--- src/modules/pottu.ts
+++ src/modules/pottu.ts
@@ -11,7 +11,7 @@ bot.on("sticker", ({ message }) => {
 })
 
 bot.hears(["pottu", "Pottu"], ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
-bot.command("pottu", ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
+bot.command(["pottu", "poika"], ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
 
 bot.hears(["Glom", "glom"], ({ replyWithSticker }) => replyWithSticker(stickers.glom))
 bot.command("glom", ({ replyWithSticker }) => replyWithSticker(stickers.glom))
