1df376cc19c4f37e11923b40e04bf0af0ff7124b
otsohellman <otso.hellman@gmail.com>
2019-11-29 12:51:11 +0200

    big fix

--- src/modules/lime.ts
+++ src/modules/lime.ts
@@ -2,7 +2,7 @@ import bot from "../bot"
 
 const INTERVAL = 30 * 60 * 1000
 
-const rand = () => Math.random() * 5 * 60 * 60 * 1000 + 30 * 60 * 1000
+const rand = () => Math.random() * 5 * 60 * 60 * 1000 + INTERVAL
 
 let nextTime = -Infinity
 bot.on("message", ({ message, reply }) => {
