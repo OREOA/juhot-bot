71a15dc790996be5641cd6f67a5368a28d3f45f1
otsohellman <otso.hellman@gmail.com>
2019-12-06 12:12:37 +0200

    hack because extra modules dont work

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Context } from "telegraf"
 
 import "./modules/pottu"
 import "./modules/lime"
-import "./modules/sussi"
 
 bot.launch()
 bot.catch((err: any) => console.log(err));
--- src/modules/pottu.ts
+++ src/modules/pottu.ts
@@ -2,12 +2,26 @@ import bot from "../bot"
 import { Context, ContextMessageUpdate } from 'telegraf'
 import stickers from "../stickers.json"
 
+const SUSSIS = ['sussi', 'sushi', 'könnitsiva', 'konnichiwa', 'konnitsiva']
+
+const containsSussi = (text: string) => {
+    for (let sussi of SUSSIS) {
+        if (text.includes(sussi)) {
+            return true
+        }
+    }
+    return false
+}
+
 const replyWithSticker = (sticker: string) => (ctx: ContextMessageUpdate) => {
     ctx.replyWithSticker(sticker)
 }
 
-bot.on("sticker", ({ message }) => {
-    const sticker = message && message.sticker
+bot.on("message", ({ message }) => {
+    const text = message && message.text
+    if (text && containsSussi(text)) {
+        replyWithSticker(stickers.glom)
+    }
 })
 
 bot.hears(["pottu", "Pottu"], ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
