f7bcc41116f07d563b0378af00e00f55592b86ff
otsohellman <otso.hellman@gmail.com>
2019-12-06 23:42:37 +0200

    add huutis

--- src/modules/lime.ts
+++ src/modules/lime.ts
@@ -3,6 +3,8 @@ import stickers from "../stickers.json"
 
 const INTERVAL = 30 * 60 * 1000
 const SUSSIS = ['sussi', 'sushi', 'könnitsiva', 'konnichiwa', 'konnitsiva', 'könnichiwa']
+const HUUTIS = ['😂', 'huutista', ':d']
+const HUUTIS_ANSWERS = ['😂😂😂😂', 'huutista', ':DDDD', 'huu', 'huutitata', ':--D']
 
 const rand = () => Math.random() * 5 * 60 * 60 * 1000 + INTERVAL
 
@@ -15,7 +17,16 @@ const containsSussi = (text: string) => {
     return false
 }
 
+const containsHuutista = (text: string) => {
+    for (let huutis of HUUTIS) {
+        if (text.toLowerCase().includes(huutis)) {
+            return true
+        }
+    }
+}
+
 let nextTime = -Infinity
+let nextHuutisTime = -Infinity
 bot.on("message", ({ message, reply, replyWithSticker }) => {
     const sender = message && message.from && message.from.username
     const now = Date.now()
@@ -26,5 +37,9 @@ bot.on("message", ({ message, reply, replyWithSticker }) => {
     const text = message && message.text
     if (text && containsSussi(text)) {
         replyWithSticker(stickers.sussi)
+    }
+    if (text && now >= nextHuutisTime && containsHuutista(text)) {
+        nextHuutisTime = now + rand()
+        reply(HUUTIS_ANSWERS[Math.floor(Math.random()*HUUTIS_ANSWERS.length)])
     }
 })
