42893ce307f22688a50d7b8a60855304100c835b
Otto Hyytiälä <otto.hyytiala@gmail.com>
2020-01-16 18:49:14 +0200

    junkyard commit

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,11 @@ const BASE_URL = process.env.BASE_URL
 const ENABLE_FREE_TEXT = process.env.ENABLE_FREE_TEXT
 const FREE_TEXT_IN_PRIVATE_ONLY = process.env.FREE_TEXT_IN_PRIVATE_ONLY
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+const AWS_KEY = process.env.AWS_KEY
+const AWS_SECRET = process.env.AWS_SECRET
 
 export default {
     TELEGRAM_TOKEN,
+    AWS_KEY,
+    AWS_SECRET
 }
--- src/modules/pottu.ts
+++ src/modules/pottu.ts
@@ -1,8 +1,22 @@
 import bot from "../bot"
 import stickers from "../stickers.json"
 
+import aws from "aws-sdk"
+
+const config = new aws.Config({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "eu-central-1"
+}) 
+
+const client = new aws.Rekognition()
+
 bot.hears(["pottu", "Pottu"], ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
 bot.command(["pottu", "poika"], ({ replyWithSticker }) => replyWithSticker(stickers.pottu))
 
 bot.hears(["Glom", "glom"], ({ replyWithSticker }) => replyWithSticker(stickers.glom))
 bot.command("glom", ({ replyWithSticker }) => replyWithSticker(stickers.glom))
+
+bot.command(["photo"], ({ reply }) => {
+  reply("Test")
+})
