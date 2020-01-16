257345c08ace09933bae431b6d1e7464697ed40f
Otto Hyytiälä <otto.hyytiala@gmail.com>
2020-01-16 19:14:16 +0200

    create db connection

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,15 @@ const BASE_URL = process.env.BASE_URL
 const ENABLE_FREE_TEXT = process.env.ENABLE_FREE_TEXT
 const FREE_TEXT_IN_PRIVATE_ONLY = process.env.FREE_TEXT_IN_PRIVATE_ONLY
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
 
 export default {
     TELEGRAM_TOKEN,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST
 }
---
+++ src/db.ts
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+import config from "./config"
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = config
+
+const database = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres'
+})
+
+module.exports = database
