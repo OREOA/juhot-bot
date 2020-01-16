62aa98ea6fa1c62d8db4adf0110532a998401e68
Otto Hyytiälä <otto.hyytiala@gmail.com>
2020-01-16 19:29:09 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ import "./modules/lime"
 bot.launch()
 db.authenticate()
 .then(() => {
-  console.log('Connection has been established successfully.');
+  console.log('DB OK')
 })
 .catch((err: any) => {
-  console.error('Unable to connect to the database:', err);
-});
+  console.error('DB ERROR', err)
+})
 console.log("Should be running?")
-bot.catch((err: any) => console.log(err));
+bot.catch((err: any) => console.log(err))
